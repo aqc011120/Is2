58cbabff5459e7010258ed0192332f15cb08dc1b
aqc011120 <149327216+aqc011120@users.noreply.github.com>
2023-10-30 00:10:07 +0100

    Add files via upload

---
+++ relecloud/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class RelecloudConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'  # Configura BigAutoField como la clave primaria predeterminada
+    name = 'relecloud'
